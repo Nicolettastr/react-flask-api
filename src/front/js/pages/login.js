e8f3a72a800df63b9417d18619710dffc4e1a1db
Nicolettastr <nicolettastruggia@hotmail.com>
2023-02-02 21:54:07 +0000

    complete excersice

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,40 +1,42 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-    const { store, actions } = useContext(Context);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const { store, actions } = useContext(Context);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const token = sessionStorage.getItem("token");
+  const navigate = useNavigate();
 
-    const handleClick = () => {
+  const handleClick = () => {
+    actions.login(username, password);
+  };
 
-        const options = {
-          method: "POST",
-          body: JSON.stringify({
-            "email": email,
-            "password": password
-          })
-        }
-
-        fetch('https://3000-4geeksacade-reactflaskh-i8fhdh2r88x.ws-eu84.gitpod.io/api/token', options)   
-        .then(response => {
-          if (response == 200) return response.json();
-          else alert("theres been an error")
-        })
-        .then()
-        .catch(Error => {
-          console.error(Error)
-        })
-
-    }
+  if (store.token && store.token != "" && store.token != undefined)
+    navigate("/");
 
   return (
     <div className="text-center mt-5">
-      <div>
-        <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <button onClick={handleClick}>Log in</button>
-      </div>
+      {store.token && store.token != "" && store.token != undefined ? (
+        `You are logged in with ${store.token}`
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleClick}>Log in</button>
+        </div>
+      )}
     </div>
   );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,105 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      token: null,
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      syncToken: () => {
+        const token = sessionStorage.getItem("token");
+        if (token && token != "" && token != undefined)
+          setStore({ token: token });
+      },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+      login: async (username, password) => {
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password,
+          }),
+        };
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        try {
+          const response = await fetch(
+            "https://3001-nicolettast-reactflaska-aybhpnhzcco.ws-eu85.gitpod.io/api/token",
+            options
+          );
+          if (response.status !== 200) {
+            alert("theres been an error");
+            return false;
+          }
+
+          const data = await response.json();
+          console.log("this came from the backend", data);
+          sessionStorage.setItem("token", data.access_token);
+          setStore({ token: data.access_token });
+          return true;
+        } catch (error) {
+          console.error("there has been an login error", error);
+        }
+      },
+
+      getMessage: () => {
+        const store = getStore();
+        const opt = {
+          headers: { Authorization: "Bearer " + store.token },
+        };
+
+        fetch(
+          "https://3001-nicolettast-reactflaska-aybhpnhzcco.ws-eu85.gitpod.io/api/hello",
+          opt
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.message);
+            setStore({ message: data.message });
+          })
+
+          .catch((error) => console.error("there is an error", error));
+      },
+
+      logout: () => {
+        sessionStorage.removeItem("token");
+        setStore({ token: null });
+      },
+
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
+
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
